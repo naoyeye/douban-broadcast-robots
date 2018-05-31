d4cb375be117ecd3b99b3adba1d43f1969134d0e
naoyeye <naoyeye@gmail.com>
2018-05-31 20:00:04 +0800

    优化面试接口

--- routes/person.js
+++ routes/person.js
@@ -2,7 +2,7 @@
 * @Author: hanjiyun
 * @Date:   2018-05-31 13:44:07
 * @Last Modified by:   hanjiyun
-* @Last Modified time: 2018-05-31 13:46:45
+* @Last Modified time: 2018-05-31 14:31:35
 */
 
 'use strict';
@@ -13,19 +13,25 @@ var AV = require('leanengine');
 
 // 新增
 router.post('/new', function(req, res, next) {
-  var content = req.body.content;
+  var data = req.body;
+  console.log('req.body.content - ', req.body)
   // var todo = new Todo();
   // todo.set('content', content);
   // todo.save().then(function(todo) {
   //   res.redirect('/todos');
   // }).catch(next);
 
-  var data = {
-    age: content.age,
-    name: content.name
+  if (!data || !data.name || !data.age) {
+    res.json({
+      error: 1,
+      message: '出错了，后端没有拿到前端传递的数据'
+    })
+  } else {
+    res.json({
+      age: data.age,
+      name: data.name
+    })
   }
-
-  res.json(data);
 });
 
 module.exports = router;
